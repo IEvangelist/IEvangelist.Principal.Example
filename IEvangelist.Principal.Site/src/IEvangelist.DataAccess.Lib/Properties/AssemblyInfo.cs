8a36ff436746c89e6894ce68f44bd86628fe6731
jci-cpined <david.pine-ext@jci.com>
2017-01-06 20:36:06 -0600

    Added class library for principal example

---
+++ IEvangelist.Principal.Site/src/IEvangelist.DataAccess.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johnson Controls")]
+[assembly: AssemblyProduct("IEvangelist.DataAccess.Lib")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c6e04520-b976-4e33-b15d-64828f9928dd")]
